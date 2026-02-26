24b26743b16de78bc4490605e2932fb97a1cade7
consigliori0609-spec <consigliori0609@gmail.com>
2026-02-26 14:31:06 +0300

    Upload

---
+++ data.js
@@ -0,0 +1,196 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0----1",
+      "name": "Вид с улицы 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.16309817465488585,
+          "pitch": 0.5679842816445699,
+          "rotation": 0,
+          "target": "0----1"
+        },
+        {
+          "yaw": 0.9833318151041901,
+          "pitch": 0.08966393378459969,
+          "rotation": 0,
+          "target": "1----2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1----2",
+      "name": "Вид с улицы 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.254250956623876,
+          "pitch": 0.6039213451721963,
+          "rotation": 0,
+          "target": "1----2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2----",
+      "name": "Вид на открытую террасу",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1156563049417976,
+          "pitch": 0.09914112863793356,
+          "rotation": 0,
+          "target": "0----1"
+        },
+        {
+          "yaw": -1.2858808110643878,
+          "pitch": 0.1161361252649975,
+          "rotation": 0,
+          "target": "1----2"
+        },
+        {
+          "yaw": -0.04187219010443677,
+          "pitch": 0.4304198393390877,
+          "rotation": 6.283185307179586,
+          "target": "2----"
+        },
+        {
+          "yaw": 3.0610098852958805,
+          "pitch": 0.33634294325294434,
+          "rotation": 0,
+          "target": "3---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3---",
+      "name": "Вид на интерьер",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
